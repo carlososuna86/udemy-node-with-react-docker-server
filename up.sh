c6f250fff7ea6e1fc195ede36cfa3dac7cf24982
Carlos Augusto Osuna <carlososuna86@gmail.com>
2020-02-28 18:08:25 -0300

    Add more verbose messages on the `up.sh` script

--- up.sh
+++ up.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "> Starting the MongoDB container"
 docker-compose up -d mongo
+
+echo "> Waiting 15 for MongoDB to startup"
 sleep 15
+
+echo "> Starting the other containers"
 docker-compose up -d
+
+echo "> Showing message logs for all containers"
 docker-compose logs -f
